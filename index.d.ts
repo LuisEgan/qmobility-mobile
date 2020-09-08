592056d5676d52ff32c4be10028d7a3532910425
L <l@cleveritgroup.com>
2020-09-08 00:42:55 -0300

    ✨ extend GooglePlacesAutocompleteProps type def

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,12 @@
+import "react-native-google-places-autocomplete";
+
 /* eslint no-empty: "off" */
 declare module "*.jpg" {}
 declare module "*.jpeg" {}
 declare module "*.png" {}
+
+declare module "react-native-google-places-autocomplete" {
+  interface GooglePlacesAutocompleteProps {
+    renderRow?: (description: DescriptionRow) => JSX.Element;
+  }
+}
