b6444b25ccc5fdb8510ffcfd82e00b1e4d9d9c04
L <l@cleveritgroup.com>
2020-09-27 10:59:26 -0300

    ✨ ICE details screen

--- src/screens/App/DetailsICE/index.tsx
+++ src/screens/App/DetailsICE/index.tsx
@@ -26,8 +26,8 @@ interface IIconText {
 const DetailsICE = () => {
   const { goBack } = useNavigation();
 
-  const { data: eVe, loading } = useQuery<{ user: IUser }, IUser>(
-    User.queries.getEve,
+  const { data, loading } = useQuery<{ user: IUser }, IUser>(
+    User.queries.getICEVehicle,
   );
 
   const IconText = ({ icon, label }: IIconText) => (
@@ -47,6 +47,8 @@ const DetailsICE = () => {
 
   if (loading) return <FullScreenModal show />;
 
+  const vehicle = data?.user.iceVehicle;
+
   return (
     <View
       style={[
@@ -57,79 +59,31 @@ const DetailsICE = () => {
       ]}
     >
       <View style={styles.goBack}>
-        <Icons icon="ArrowBack" fill="white" onPress={goBack} />
+        <Icons icon="ArrowBack" onPress={goBack} />
       </View>
 
       <ScrollView style={styles.containerScroll}>
         <View style={styles.contentTitle}>
           <Text variant="heading2">
-            {eVe?.user.selectedVehicle?.Vehicle_Make}
+            {vehicle?.Make}
             {" "}
-            {eVe?.user.selectedVehicle?.Vehicle_Model}
+            {vehicle?.MakeModel}
           </Text>
           <TouchableOpacity>
             <Icons icon="MoreVert" fill={theme.colors.primary} size={28} />
           </TouchableOpacity>
         </View>
 
-        <View style={styles.content}>
-          <Text
-            variant="bodyHighlightBold"
-            style={{
-              marginRight: 10,
-            }}
-          >
-            eVe Battery
-            {" "}
-            {eVe?.user.selectedVehicle?.Battery_Capacity_Full}
-            {" "}
-            kWh
-          </Text>
-          <Text variant="bodyBold">
-            Range
-            {" "}
-            {eVe?.user.selectedVehicle?.Range_Real}
-            {" "}
-            km
-          </Text>
-        </View>
-
         <View style={[styles.content]}>
-          <IconText
-            icon="Bubble"
-            label={`${eVe?.user.selectedVehicle?.Misc_Seats}`}
-          />
-          <IconText icon="Spa" label="0 C02g/km" />
-        </View>
-
-        <View style={[styles.content]}>
-          <IconText
-            icon="Polymer"
-            label={`Max ${eVe?.user.selectedVehicle?.Range_Real} km`}
-          />
-          <IconText
-            icon="Nature"
-            label={`${eVe?.user.selectedVehicle?.Efficiency_Real} kWh/100km`}
-          />
-        </View>
-
-        <View style={[styles.content]}>
-          <IconText
-            icon="Speed"
-            label={`Max ${eVe?.user.selectedVehicle?.Performance_Topspeed}  km/h`}
-          />
-          <IconText
-            icon="Flash"
-            label={`Time ${eVe?.user.selectedVehicle?.Fastcharge_ChargeTime} mins`}
-          />
+          <IconText icon="Bubble" label={`${vehicle?.SeatingCapacity}`} />
+          <IconText icon="Spa" label={`${vehicle?.Co2Emissions} C02g/km`} />
         </View>
 
         <View style={[styles.content, { justifyContent: "space-between" }]}>
           <Text variant="bodyBold">United Kingdom</Text>
           <Text variant="bodyBold">
             £
-            {" "}
-            {numberWithDots(`${eVe?.user.selectedVehicle?.Price_From_UK}`)}
+            {numberWithDots("0")}
           </Text>
         </View>
 
@@ -142,9 +96,7 @@ const DetailsICE = () => {
           >
             Availability
           </Text>
-          <Text variant="body">
-            {eVe?.user.selectedVehicle?.Availability_Status}
-          </Text>
+          <Text variant="body" />
         </View>
       </ScrollView>
     </View>
@@ -174,52 +126,13 @@ const styles = StyleSheet.create({
     marginVertical: "3%",
   },
   goBack: {
-    position: "absolute",
-    top: height * 0.05,
-    left: width * 0.03,
     zIndex: 1,
+    marginTop: height * 0.05,
+    marginLeft: width * 0.03,
   },
   iconTextContent: {
     flexDirection: "row",
     marginVertical: "1%",
     marginRight: 15,
-  },
-  line: {
-    borderLeftWidth: 1,
-    height: 60,
-    alignSelf: "flex-end",
-    marginBottom: 10,
-  },
-  card: {
-    marginVertical: "5%",
-    borderRadius: 10,
-    borderWidth: 1,
-    height: 122,
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  cardContent: {
-    justifyContent: "center",
-    flex: 1,
-    alignItems: "center",
-  },
-  cardBody: {
-    width: "100%",
-    alignItems: "center",
-  },
-  cardBodyContent: {
-    flexDirection: "row",
-    marginTop: "10%",
-    justifyContent: "center",
-  },
-  cardType: {
-    alignSelf: "flex-end",
-    opacity: 0.25,
-  },
-  button: {
-    marginHorizontal: "5%",
-    marginBottom: "6%",
-    marginTop: "3%",
-    justifyContent: "center",
   },
 });
